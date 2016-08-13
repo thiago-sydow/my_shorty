f6a6b2719eedc4c1172675122bc4b0c6b63577c3
Thiago von Sydow <thiagovs05@gmail.com>
2016-08-13 17:33:41 -0300

    added a memory repository to use in specs and development for now

---
+++ app/repositories/in_memory_repository.rb
@@ -0,0 +1,23 @@
+class InMemoryRepository < BaseRepository
+
+  def initialize
+    @records = Hash.new
+  end
+
+  def find(code)
+    @records[code]
+  end
+
+  def create_short_code(code, url)
+    @records[code] = {
+      url: url,
+      start_date: Time.now,
+      redirect_count: 0,
+      last_seen_date: nil
+    }
+  end
+
+  def exists?(code)
+    @records.has_key?(code)
+  end
+end
