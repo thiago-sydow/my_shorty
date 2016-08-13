fd8153246ebfb3eb848d8f78137e09658c534344
Thiago von Sydow <thiagovs05@gmail.com>
2016-08-13 12:31:05 -0300

    added helper to handle json responses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,12 @@ require File.expand_path '../config/environment.rb', __dir__
 
 # Support files
 require File.expand_path './support/rspec_mixin.rb', __dir__
+require File.expand_path './support/json_helpers.rb', __dir__
 
 RSpec.configure do |config|
   # Disable usage of monkey patch version of describe
   # by preventing global injection of RSpec's DSL
   config.expose_dsl_globally = false
-  config.include RSpecMixin
+  config.include RSpecMixin, type: :request
+  config.include JsonHelpers, type: :request
 end
